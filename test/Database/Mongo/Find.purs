a1c56c86f89aa2eaab3dd937c44ef594fafe890d
Allan Lukwago <epicallan.al@gmail.com>
2017-05-09 18:57:41 +0300

    update for purescript 0.11

--- test/Database/Mongo/Find.purs
+++ test/Database/Mongo/Find.purs
@@ -1,25 +1,32 @@
 module Test.Database.Mongo.Find where
 
-import Prelude (Unit, bind, (>), ($))
-import Data.Array (length)
-import Data.Maybe (Maybe(..))
-import Database.Mongo.Bson.BsonValue ((:=))
+import Control.Alt ((<|>))
+import Control.Monad.Aff (attempt)
+import Control.Monad.Aff.Console (logShow)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log)
-import Test.Data.Event (Event(..))
+import Control.Monad.Eff.Exception (Error)
+import Data.Argonaut (encodeJson)
+import Data.Array (length)
+import Data.Either (either, Either)
+import Data.Maybe (Maybe(..))
+import Data.Unit (unit)
+import Database.Mongo.Bson.BsonValue ((:=))
+import Database.Mongo.Mongo (Database, collect, collection, find, findOne)
+import Prelude (Unit, bind, const, pure, show, unit, ($), (>))
 import Test.Assert (assert)
-import Database.Mongo.Mongo (Database, collect, find, collection)
+import Test.Data.Event (Event(..))
 import Test.Type (Test)
 
 evt :: Event
 evt = Event
-  { name : Just "Wow"
+  { name : Nothing
   }
 
 main :: Database -> Test Unit
 main database = do
   liftEff $ log "should find"
   col <- collection "events" database
-  cur <- find [ "name" := "Wow" ] [ "name" := 1.0 ] col
-  res <- collect cur
-  liftEff $ assert $ length (res :: Array Event) > 0
+  (e :: Either Error Event) <- attempt $ findOne [ "name" := "Wow" ] [] col
+  liftEff $ either (const $ log "Oh noes!") (const $ log "Yays!") e
+  pure unit
